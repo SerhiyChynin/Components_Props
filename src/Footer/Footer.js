86199af1c3403053cf9e1a7f6521b62064037605
SerhiiChynin <sergey.chinin@gmail.com>
2023-02-06 15:02:05 +0200

    Component

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import "./Footer.css"
+
+
+function Footer() {
+    return (
+        <div className="Footer-site-footer">
+            <b>React site</b>
+            <p>2023 Copyright. All rights reserved</p>
+        </div>
+    ); 
+}
+
+export default Footer;
---
+++ src/Main/Main.js
@@ -0,0 +1,16 @@
+import './Main.css'
+import articles from '../data/articles.json' 
+
+function Main() {
+    return (
+        <div className='Main-site-main'>
+            {articles.map(item =>
+                <section>
+                    <h2>{item.title}</h2>
+                    <div>{item.body}</div>
+                </section>)}
+        </div>
+    ); 
+}
+
+export default Main;
